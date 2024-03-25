98fb6d99beef1495090a63236da8ca6562efc234
UmerAhmad9126 <ahmadumer9126@gmail.com>
2024-03-26 03:20:57 +0530

    header section added

--- chatifyApp/App.tsx
+++ chatifyApp/App.tsx
@@ -1,10 +1,12 @@
-import { View } from 'react-native'
+import { View, StatusBar } from 'react-native'
 import React from 'react'
 import HomeScreen from './src/screen/HomeScreen'
+import { Colors } from './src/theme/Colors'
 
 const App = () => {
   return (
     <View>
+      <StatusBar backgroundColor={Colors.primaryColor} />
       <HomeScreen />
     </View>
   )
--- chatifyApp/src/components/Header.jsx
+++ chatifyApp/src/components/Header.jsx
@@ -1,10 +1,41 @@
-import { View, Text } from 'react-native'
+import { View, Image, StyleSheet } from 'react-native'
 import React from 'react'
+import chatifyLogo from "../assets/WhatsApp_logo.png"
+import { Colors } from '../theme/Colors'
+import VectorIcon from '../utils/VectorIcons'
 
 export default function Header() {
     return (
-        <View>
-            <Text>Header</Text>
+        <View style={styles.container}>
+            <Image source={chatifyLogo} style={styles.chatifyLogo} />
+
+            <View style={styles.headerIcon}>
+                <VectorIcon type="Feather" name="camera" color={Colors.secondaryColor} size={22} />
+                <VectorIcon type="Ionicons" name="search" color={Colors.secondaryColor} size={20} style={styles.iconStyle} />
+                <VectorIcon type="Entypo" name="dots-three-vertical" color={Colors.secondaryColor} size={18} />
+            </View>
         </View>
     )
 }
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: Colors.primaryColor,
+        padding: 16,
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    chatifyLogo: {
+        width: 110,
+        height: 25,
+    },
+    headerIcon: {
+        flexDirection: "row",
+
+    },
+    iconStyle: {
+        marginHorizontal: 25
+    }
+})  
